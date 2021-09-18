c273382fd6cdb94823e585cd29f0596f7734fd94
Volodymyr-Fialkovskyi <90956281+Volodymyr-Fialkovskyi@users.noreply.github.com>
2021-09-18 11:17:43 +0300

    Add files via upload

---
+++ src/com/javarush/test/start.java
@@ -0,0 +1,81 @@
+package com.javarush.test;
+
+public class start {
+    public static void main(String[] args) {
+        int a = 21, b = 17;
+        int Plus, Minus, Mn, Dil;
+        Plus = a + b;
+        Minus = a - b;
+        Mn = a * b;
+        Dil = a / b;
+        System.out.println(Plus);
+        System.out.println(Minus);
+        System.out.println(Mn);
+        System.out.println(Dil);
+    }
+}
+
+class Byte {
+
+    public static void main(String[] args) {
+        byte a = 100, b = 5;
+        byte Plus, Minus, Mn, Dil;
+        Plus = (byte) (a + b);
+        Minus = (byte) (a - b);
+        Mn = (byte) (a * b);
+        Dil = (byte) (a / b);
+        System.out.println(Plus);
+        System.out.println(Minus);
+        System.out.println(Mn);
+        System.out.println(Dil);
+    }
+}
+
+class Short {
+
+    public static void main(String[] args) {
+        short a = 595, b = 350;
+        short Plus, Minus, Mn, Dil;
+        Plus = (short) (a + b);
+        Minus = (short) (a - b);
+        Mn = (short) (a * b);
+        Dil = (short) (a / b);
+        System.out.println(Plus);
+        System.out.println(Minus);
+        System.out.println(Mn);
+        System.out.println(Dil);
+    }
+}
+
+class Double {
+
+    public static void main(String[] args) {
+        double a = 43.95D, b = 15.3D;
+        double Plus, Minus, Mn, Dil;
+        Plus = a + b;
+        Minus = a - b;
+        Mn = a * b;
+        Dil = a / b;
+        System.out.println(Plus);
+        System.out.println(Minus);
+        System.out.println(Mn);
+        System.out.println(Dil);
+    }
+}
+
+class Float {
+
+    public static void main(String[] args) {
+        float a = 37.8F, b = 25.5F;
+        float Plus, Minus, Mn, Dil;
+        Plus = a + b;
+        Minus = a - b;
+        Mn = a * b;
+        Dil = a / b;
+        System.out.println(Plus);
+        System.out.println(Minus);
+        System.out.println(Mn);
+        System.out.println(Dil);
+    }
+}
+
